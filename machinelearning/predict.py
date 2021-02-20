afd967e6d9559fd8cca4f01f27ab0e373b881598
amit076 <amittimalsina14@gmail.com>
2021-02-20 19:28:53 +0545

    Minor edits

--- machinelearning/predict.py
+++ machinelearning/predict.py
@@ -19,9 +19,9 @@ def augment_image(img_path):
 
 module_dir = os.path.dirname(__file__)
 
-json_dir = "/home/alpha/Hackathon/Forum/machinelearning/model/model.json"
-model_dir = "/home/alpha/Hackathon/Forum/machinelearning/model/model.h5"
-csv_dir = "/home/alpha/Hackathon/Forum/machinelearning/model/model.csv"
+json_dir = module_dir + "/model/model.json"
+model_dir = module_dir + "/model/model.h5"
+csv_dir = module_dir + "/model/model.csv"
 
 Jfile = open(json_dir)
 json_read = json.load(Jfile)
@@ -31,7 +31,7 @@ model = load_model(model_dir)
 df = pd.read_csv(csv_dir)
 
 def predict(df, img):
-    img = augment_image("/home/alpha/Hackathon/Forum" + img)
+    img = augment_image("/home/projects/KU/forum" + img)
     pred = model.predict(img)
     disease = np.where(pred == pred.max())[1][0]
     output = [df.loc[disease].Disease, df.loc[disease].Symptom, df.loc[disease].Treatment]
